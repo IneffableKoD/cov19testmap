2de538c1704c89a64d3ce08d5eadd55f3646a614
IneffableKoD <yannick.zehnder@gmai.com>
2020-03-29 05:06:28 +0200

    rearrange scripts

--- docs/js/scirpt_facilities.js
+++ docs/js/scirpt_facilities.js
@@ -1,5 +1,17 @@
 //JS leaflet 
 
+var map = L.map('map', {
+    center: [46.8182, 8.2275], 
+    zoom: 5,  // 1-18
+    scrollWheelZoom: true
+      });
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(map);
+
+/*
 var testFacilityIcon = new
 L.icon({
     iconUrl: '../img/Icon_Testing_Facility_C.png',
@@ -11,8 +23,9 @@ L.icon({
     //shadowAnchor: [22, 94]
 });
 L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
+*/
 
-
+/*
 
 functionn infoPopup (feature, layer) {
     layer.bindPopup("<h1 class='infoHeader'>Testing Facility Info</h1><p class='infoHeader'>" + feature.properties.adress +"</p>");
@@ -20,9 +33,10 @@ functionn infoPopup (feature, layer) {
 
 };
 
+*/
 
-L.geoJSON(test_sites_ch,{
+L.geoJSON(testSitesCHE,{
 
-    onEachFeature: infoPopup
+    //onEachFeature: infoPopup
         
 }).addTo(map);
