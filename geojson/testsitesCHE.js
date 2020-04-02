8f8a26a3c480cb3f35b65c7719501eff232af312
IneffableKoD <34814092+IneffableKoD@users.noreply.github.com>
2020-04-02 23:26:02 +0200

    update with prod

---
+++ geojson/testsitesCHE.js
@@ -0,0 +1,74 @@
+var testSitesCHE =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "UKBB",
+        "address": "Spitalstrasse 33, 4056 Basel, Switzerland",
+        "contact": "+41 61 704 12 12",
+        "web": "https://www.ukbb.ch/",
+        "comment": "Only kids younger than 16 are tested"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.581703662872314,
+          47.56356134342556
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "KUSPO Muenchenstein",
+        "address": "Loogstrasse 2, 4142 Muenchenstein, Switzerland",
+        "contact": "-",
+        "web": "-",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.616620659828186,
+          47.5204177883841
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Mehrzweckhalle Stutz",
+        "address": "Stutzstrasse 1, 4415 Lausen, Switzerland",
+        "contact": "-",
+        "web": "-",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.764571309089661,
+          47.47298195871847
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kantonsschule Alpenquai",
+        "address": "Alpenquai 46-50, 6005 Luzern",
+        "contact": "-",
+        "web": "https://www.pilatustoday.ch/coronavirus/drive-in-coronavirus-tests-im-kanton-luzern-137330917",
+        "comment": "Drive-in test, only by doctors appointement."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.321768045425415,
+          47.043969390128844
+        ]
+      }
+    }
+  ]
+}
