bd074991596ae90f8e70b087feaed6283b5c5da9
IneffableKoD <34814092+IneffableKoD@users.noreply.github.com>
2020-04-06 22:07:12 +0200

    add CZE facilities

--- geojson/testsitesCZE.js
+++ geojson/testsitesCZE.js
@@ -19,5 +19,90 @@ var testsitesCZE =
         ]
       }
     },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nemocnice Na Bulovce",
+        "address": "Budínova 67/2, 180 81 Praha 8 – Libeň, Czechia",
+        "contact": "266082642",
+        "web": "https://koronavirus.mzcr.cz/nemocnice-na-bulovce/",
+        "comment": "Provozní doba	pracovní dny 7 – 15 hodin víkend 11 – 19 hodin na odběry je možné on-line objednání. Upřesnění polohy	kontejner u budovy Kliniky infekčních, parazitárních a tropických nemocí, mezi pavilony 7 a 8"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.464082,
+          50.115381
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "EUC Laboratoře",
+        "address": "Veleslavínská 1/30, 162 00 Praha 6, Czechia",
+        "contact": "725572830",
+        "web": "https://koronavirus.mzcr.cz/euc-laboratore/",
+        "comment": "Provozní doba	pondělí – pátek 8 – 12 hodin. Upřesnění polohy	Zámek Veleslavín, bez objednání"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.350785,
+          50.09326
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fakultní nemocnice Královské Vinohrady",
+        "address": "Šrobárova 1150/50, 100 34 Praha 10, Czechia",
+        "contact": "771136124",
+        "web": "https://koronavirus.mzcr.cz/fakultni-nemocnice-kralovske-vinohrady/",
+        "comment": "Provozní doba	neděle – pátek 7.30 – 16 hodin na odběry je možné pouze on-line objednání. Upřesnění polohy	červené stany vedle budovy L – ředitelství, drive-in"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.476464,
+          50.074236
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Městská poliklinika Praha, Výstaviště Praha",
+        "address": "Výstaviště 67, 170 00 Praha 7, Czechia",
+        "contact": "-",
+        "web": "https://koronavirus.mzcr.cz/mestska-poliklinika-praha",
+        "comment": "Provozní doba	pondělí – pátek 8 – 18 hodin. Upřesnění polohy	za plaveckým bazénem, pouze drive-in"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.430489,
+          50.104681
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fakultní nemocnice Olomouc",
+        "address": "I. P. Pavlova 185/6, 77900 Olomouc, Czechia",
+        "contact": "739598026",
+        "web": "https://koronavirus.mzcr.cz/fakultni-nemocnice-olomouc",
+        "comment": "Provozní doba	denně 8 – 14 hodin od 10. do 13. 4. budou odběry probíhat v době 8 – 12 hodin. Upřesnění polohy	Hněvotínská 3, horní plocha parkoviště pod Teoretickými ústavy LF UP"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.238215,
+          49.583681
+        ]
+      }
+    },
   ]
 }
