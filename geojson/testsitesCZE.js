797ecc694d52afdbbc9ac4f0c99b44dab9284f81
IneffableKoD <34814092+IneffableKoD@users.noreply.github.com>
2020-04-06 23:19:49 +0200

    append CZE facilities

--- geojson/testsitesCZE.js
+++ geojson/testsitesCZE.js
@@ -9,7 +9,7 @@ var testsitesCZE =
         "address": "U Vojenské nemocnice 1200, 169 02 Praha 6, Czechia",
         "contact": "973208333",
         "web": "https://koronavirus.mzcr.cz/ustredni-vojenska-nemocnice",
-        "comment": "Provozní doba	pracovní dny 8 – 18 hodin, víkend 8 – 15 hodin. Upřesnění polohy	před areálem ÚVN ve Stamicově ulici"
+        "comment": "Provozní doba	pracovní dny 8 – 18 hodin, víkend 8 – 15 hodin. Upřesnění polohy	před areálem ÚVN ve Stamicově ulici."
       },
       "geometry": {
         "type": "Point",
@@ -26,7 +26,7 @@ var testsitesCZE =
         "address": "Budínova 67/2, 180 81 Praha 8 – Libeň, Czechia",
         "contact": "266082642",
         "web": "https://koronavirus.mzcr.cz/nemocnice-na-bulovce/",
-        "comment": "Provozní doba	pracovní dny 7 – 15 hodin víkend 11 – 19 hodin na odběry je možné on-line objednání. Upřesnění polohy	kontejner u budovy Kliniky infekčních, parazitárních a tropických nemocí, mezi pavilony 7 a 8"
+        "comment": "Provozní doba	pracovní dny 7 – 15 hodin víkend 11 – 19 hodin na odběry je možné on-line objednání. Upřesnění polohy	kontejner u budovy Kliniky infekčních, parazitárních a tropických nemocí, mezi pavilony 7 a 8."
       },
       "geometry": {
         "type": "Point",
@@ -43,7 +43,7 @@ var testsitesCZE =
         "address": "Veleslavínská 1/30, 162 00 Praha 6, Czechia",
         "contact": "725572830",
         "web": "https://koronavirus.mzcr.cz/euc-laboratore/",
-        "comment": "Provozní doba	pondělí – pátek 8 – 12 hodin. Upřesnění polohy	Zámek Veleslavín, bez objednání"
+        "comment": "Provozní doba	pondělí – pátek 8 – 12 hodin. Upřesnění polohy	Zámek Veleslavín, bez objednání."
       },
       "geometry": {
         "type": "Point",
@@ -60,7 +60,7 @@ var testsitesCZE =
         "address": "Šrobárova 1150/50, 100 34 Praha 10, Czechia",
         "contact": "771136124",
         "web": "https://koronavirus.mzcr.cz/fakultni-nemocnice-kralovske-vinohrady/",
-        "comment": "Provozní doba	neděle – pátek 7.30 – 16 hodin na odběry je možné pouze on-line objednání. Upřesnění polohy	červené stany vedle budovy L – ředitelství, drive-in"
+        "comment": "Provozní doba	neděle – pátek 7.30 – 16 hodin na odběry je možné pouze on-line objednání. Upřesnění polohy	červené stany vedle budovy L – ředitelství, drive-in."
       },
       "geometry": {
         "type": "Point",
@@ -77,7 +77,7 @@ var testsitesCZE =
         "address": "Výstaviště 67, 170 00 Praha 7, Czechia",
         "contact": "-",
         "web": "https://koronavirus.mzcr.cz/mestska-poliklinika-praha",
-        "comment": "Provozní doba	pondělí – pátek 8 – 18 hodin. Upřesnění polohy	za plaveckým bazénem, pouze drive-in"
+        "comment": "Provozní doba	pondělí – pátek 8 – 18 hodin. Upřesnění polohy	za plaveckým bazénem, pouze drive-in."
       },
       "geometry": {
         "type": "Point",
@@ -94,7 +94,7 @@ var testsitesCZE =
         "address": "I. P. Pavlova 185/6, 77900 Olomouc, Czechia",
         "contact": "739598026",
         "web": "https://koronavirus.mzcr.cz/fakultni-nemocnice-olomouc",
-        "comment": "Provozní doba	denně 8 – 14 hodin od 10. do 13. 4. budou odběry probíhat v době 8 – 12 hodin. Upřesnění polohy	Hněvotínská 3, horní plocha parkoviště pod Teoretickými ústavy LF UP"
+        "comment": "Provozní doba	denně 8 – 14 hodin od 10. do 13. 4. budou odběry probíhat v době 8 – 12 hodin. Upřesnění polohy	Hněvotínská 3, horní plocha parkoviště pod Teoretickými ústavy LF UP."
       },
       "geometry": {
         "type": "Point",
@@ -104,5 +104,296 @@ var testsitesCZE =
         ]
       }
     },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vojenská nemocnice Olomouc",
+        "address": "Sušilovo náměstí 5, 771 11 Olomouc, Czechia",
+        "contact": "724379435",
+        "web": "https://koronavirus.mzcr.cz/vojenska-nemocnice-olomouc/",
+        "comment": "Provozní doba	denně 9 – 15 hodin. Upřesnění polohy	parkoviště pod Klášterním Hradiskem."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.265130,
+          49.605567
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nemocnice Prostějov, Středomoravská nemocniční a. s.",
+        "address": "Mathonova 291, 796 04 Prostějov, Czechia",
+        "contact": "725851693",
+        "web": "https://koronavirus.mzcr.cz/nemocnice-prostejov/",
+        "comment": "Provozní doba	pondělí až pátek 8 – 14.30 hodin. Upřesnění polohy	stan v areálu nemocnice."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.088307,
+          49.475678
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nemocnice Šternberk, Středomoravská nemocniční a. s.",
+        "address": "Jívanská 20, 785 01 Štenberk, Czechia",
+        "contact": "587800111",
+        "web": "https://koronavirus.mzcr.cz/nemocnice-sternberk/",
+        "comment": "Provozní doba	7 – 15.30 hodin."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.297240,
+          49.722230
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nemocnice Přerov, Středomoravská nemocniční a. s.",
+        "address": "Dvořákova 1800/75, 750 02, Přerov, Czechia",
+        "contact": "581271591",
+        "web": "https://koronavirus.mzcr.cz/nemocnice-prerov/",
+        "comment": "Provozní doba	pondělí až pátek 8 – 14.30 hodin. Upřesnění polohy	odběrový stan."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.473665,
+          49.456873
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nemocnice Šumperk, a. s.",
+        "address": "Nerudova 640/41, 787 01 Šumperk, Czechia",
+        "contact": "724256083",
+        "web": "https://koronavirus.mzcr.cz/nemocnice-sumperk/",
+        "comment": "Provozní doba	7 – 14.30 hodin. Upřesnění polohy	parkoviště před vjezdem do nemocnice."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          16.967541,
+          49.959567
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jesenická nemocnice, a. s.",
+        "address": "Lipovská 103/39, 790 01 Jeseník, Czechia",
+        "contact": "584458411",
+        "web": "https://koronavirus.mzcr.cz/jesenicka-nemocnice/",
+        "comment": "Provozní doba	nonstop. See web for more information."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.198334,
+          50.227779
+        ]
+      }
+    },
+    /*
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "JOdběrové místo Litovel – praporní obvaziště pro uzavřenou oblast Litovelska a Uničovska",
+        "address": "-",
+        "contact": "-",
+        "web": "https://koronavirus.mzcr.cz/odberove-misto-litovel/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          17.198334,
+          50.227779
+        ]
+      }
+    },
+    */
+   {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice Hranice, a. s.",
+      "address": "Zborovská 1245, 753 22 Hranice, Czechia",
+      "contact": "581679385",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-hranice",
+      "comment": "Provozní doba	9 – 14 hodin. Upřesnění polohy	infekční oddělení."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        17.744860,
+        49.555300
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Fakultní nemocnice Ostrava",
+      "address": "17. listopadu 1790/5, 708 52 Ostrava-Poruba, Czechia",
+      "contact": "703168466",
+      "web": "https://koronavirus.mzcr.cz/fakultni-nemocnice-ostrava",
+      "comment": "Provozní doba	7 – 19 hodin. Upřesnění polohy	vstup z ulice K Myslivně."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.161081,
+        49.826829
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Slezská nemocnice v Opavě, p. o.",
+      "address": "Olomoucká 470/86, 746 01 Opava, Czechia",
+      "contact": "553766100",
+      "web": "https://koronavirus.mzcr.cz/slezska-nemocnice-v-opave",
+      "comment": "Provozní doba	nonstop. Upřesnění polohy	příjezd k odběrovému centru vlastním vozem."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        17.879341,
+        49.933793
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice s poliklinikou Havířov, p. o.",
+      "address": "Dělnická 1132/24, 736 01 Havířov, Czechia",
+      "contact": "596383500",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-s-poliklinikou-havirov",
+      "comment": "Provozní doba	7 – 14 hodin. Upřesnění polohy	u budovy infekčního pavilonu, vstup z ulice Moskevská."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.437993,
+        49.784522
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Městská nemocnice Ostrava – Fifejdy",
+      "address": "Nemocniční 898/20a, Moravská Ostrava, 702 00 Ostrava, Czechia",
+      "contact": "596194877",
+      "web": "https://koronavirus.mzcr.cz/mestska-nemocnice-ostava-fifejdy/",
+      "comment": "Provozní doba	8 – 16 hodin. Upřesnění polohy	Mobilní stan se dvěma unimobuňkami je umístěn za správní budovou v areálu nemocnice. Vjezd do areálu je určen z ulice Varenská a výjezd na ulici Hornopolní. Správná cesta do areálu i ven je označena šipkami."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.274675,
+        49.833069
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice Třinec, p. o.",
+      "address": "Kaštanová 268, Dolní Líštná, 739 61 Třinec, Czechia",
+      "contact": "558309790, 739044699",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-trinec/",
+      "comment": "Provozní doba	po – pá 8 – 16 hodin, so – ne 8 – 12 hodin."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.690723,
+        49.674776
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Sdružené zdravotnické zařízení Krnov, p. o.",
+      "address": "I. P. Pavlova 552/9, Pod Bezručovým vrchem, 794 01 Krnov, Czechia",
+      "contact": "703450217",
+      "web": "https://koronavirus.mzcr.cz/sdruzene-zdravotnicke-zarizeni-krnov/",
+      "comment": "Provozní doba	8 – 20 hodin. Upřesnění polohy	napravo za hlavním vjezdem do areálu SZZ Krnov, poté vyznačeno značkami."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        17.687460,
+        50.095143
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice ve Frýdku-Místku, p. o.",
+      "address": "E. Krásnohorské 321, Frýdek, 738 01 Frýdek-Místek, Czechia",
+      "contact": "558309701",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-ve-frydku-mistku/",
+      "comment": "Provozní doba	10 – 16 hodin."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.355411,
+        49.690008
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice s poliklinikou Karviná, p. o.",
+      "address": "Vydmuchov 399/5, 734 01 Karviná-Ráj, Czechia",
+      "contact": "735764885",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-s-poliklinikou-karvina/",
+      "comment": "Provozní doba	8 – 16 hodin."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.549907,
+        49.848332
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "name": "Nemocnice Nový Jičín, a. s.",
+      "address": "Purkyňova 2138/16, 74101 Nový Jičín, Czechia",
+      "contact": "739327329",
+      "web": "https://koronavirus.mzcr.cz/nemocnice-novy-jicin/",
+      "comment": "Provozní doba	pondělí až pátek 8 – 16 hodin, sobota a neděle 8 – 12 hodin."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        18.006759,
+        49.589955
+      ]
+    }
+  },
   ]
 }
