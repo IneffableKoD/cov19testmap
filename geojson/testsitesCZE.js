2d6401c0e0221777bb8fe830d631732d56564878
IneffableKoD <34814092+IneffableKoD@users.noreply.github.com>
2020-04-06 16:21:11 +0200

    add geojson CZE facilities

---
+++ geojson/testsitesCZE.js
@@ -0,0 +1,23 @@
+var testsitesCZE =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ústřední vojenská nemocnice",
+        "address": "U Vojenské nemocnice 1200, 169 02 Praha 6, Czechia",
+        "contact": "973208333",
+        "web": "https://koronavirus.mzcr.cz/ustredni-vojenska-nemocnice",
+        "comment": "Provozní doba	pracovní dny 8 – 18 hodin, víkend 8 – 15 hodin. Upřesnění polohy	před areálem ÚVN ve Stamicově ulici"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          14.363437,
+          50.090157
+        ]
+      }
+    },
+  ]
+}
