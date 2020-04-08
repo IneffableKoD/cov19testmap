2b74bc786992646f222ea4d17a5e67ba39bc9264
Yannick Zehnder <34814092+IneffableKoD@users.noreply.github.com>
2020-04-09 00:48:57 +0200

    add ZAF testing facilities

---
+++ geojson/testsitesZAF.js
@@ -0,0 +1,142 @@
+var testsitesZAF =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Parkview Clinic",
+        "address": "Corner of 14th and 5th Avenue, Parkhurst, Johannesburg, South Africa",
+        "contact": "011 788 1526/7",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0,
+          0
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "​Weltevreden Park Clinic",
+        "address": "Cnr J G Strydom & Jim Fouche Avenue, Weltevredenpark, Roodepoort, 1709, South Africa",
+        "contact": "011 675 3038",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.933308,
+          -26.119266
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "​Cosmo City Multipurpose Centre",
+        "address": "154 Angola Ave, Cosmo City, Roodepoort, 2188, South Africa",
+        "contact": "082 857 5973",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.927363,
+          -26.018626
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "​Braamfischer: Multipurpose Centre",
+        "address": "Phase 2, Braamfischer, Roodepoort, 1725, South Africa",
+        "contact": "082 818 6575",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.854220,
+          -26.203690
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "​Eastbank Clinic",
+        "address": "Cnr Springbok Crescent & Impala Street, Alexandra, 2090, South Africa",
+        "contact": "011 882 0905",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.110224,
+          -26.106702
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "​Sandown",
+        "address": "139 West Street, corner of Maud Street, Sandown, South Africa",
+        "contact": "011 881 6151/6215",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.055817,
+          -26.106343
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Halfway House",
+        "address": "Erf 18 and 19, Market Street and Moritz Street, Halfway House, South Africa",
+        "contact": "011 805 3112",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0,
+          0
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Diepsloot Clinic",
+        "address": "Erf 5355, J B Marks Drive, Diepsloot Ext 7, South Africa",
+        "contact": "011 464 5396",
+        "web": "https://northeasterntribune.co.za/264190/covid-19-city-joburg-identifies-several-clinics-screen-coronavirus/",
+        "comment": "-"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          0,
+          0
+        ]
+      }
+    },
+  ]
+}
