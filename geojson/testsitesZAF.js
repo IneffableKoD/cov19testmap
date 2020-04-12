ebac20f87c3cdcd2cabe5c1b4548f75aa4bbf0af
elolelo <lelonompumelelo@gmail.com>
2020-04-12 09:16:42 +0200

    added SA files and data

--- geojson/testsitesZAF.js
+++ geojson/testsitesZAF.js
@@ -173,22 +173,426 @@ var testsitesZAF =
         ]
       }
     },
+
+    // data from covid19za starts here:   
     {
       "type": "Feature",
       "properties": {
-        "name": "Ampath Pickering Street Laboratory",
-        "address": "13 Pickering Street, Newton Park, Port Elizabeth, South Africa",
-        "contact": "+27 41 396 9513",
-        "web": "https://www.ampath.co.za/covid-19-collection-sites",
-        "comment": "Forms required - https://www.ampath.co.za/covid-19-patients-resources"
+        "name": "Livingstone Hospital",
+        "address": "Lindsay Rd, Industrial, Port Elizabeth, 6020,South Africa",
+        "contact": "+27 41 405 9111",
+        "web": "https://en.wikipedia.org/wiki/Livingstone_Hospital",
+        "comment": "No appointment needed prior. Doctors Covid-19 test referral note is a requirement ",
+        "source": "https://sacoronavirus.co.za/contact/"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          25.571405,
-          -33.947491 
+          25.57074,
+          -33.9251
         ]
       }
     },
+    
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Charlotte Maxeke Hospital",
+        "address": " Parktown, Johannesburg, 2193, South Africa",
+        "contact": "+27 11 488 4911",
+        "web": "https://en.wikipedia.org/wiki/Charlotte_Maxeke_Johannesburg_Academic_Hospital",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.04691,
+          -26.174
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Steve Biko Academic Hospital",
+        "address": "Steve Biko Road &, Malan St, Prinshof 349-Jr, Pretoria, 0002, South Africa",
+        "contact": "+27 12 354 1000",
+        "web": "https://www.sbah.org.za/",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.202561,
+          -25.7295
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tembisa Hospital",
+        "address": " 539-541 Reverend R.T.J. Namane Dr, Hospital View, Tembisa, 1632, South Africa",
+        "contact": "+27 11 923 2000",
+        "web": "https://www.facebook.com/Tembisa-Hospital-138031559620194/",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          28.23756 ,
+          -25.9828
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grey's Hospital",
+        "address": "The Msunduzi, Town Hill, Pietermaritzburg, 3201, South Africa",
+        "contact": "+27 33 897 3000",
+        "web": "http://www.kznhealth.gov.za/greyshospital.htm",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          30.363784,
+          -29.5796
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Polokwane Hospital",
+        "address": "Dorp St, Hospital Park, Polokwane, 0699, SOuth Africa",
+        "contact": "+27 15 287 5000",
+        "web": "",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          29.440567,
+          -23.799443
+
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rob Ferreira Hospital",
+        "address": "Corner Piet Retief Street and Madiba Drive, Nelspruit, Mpumalanga, South Africa",
+        "contact": "	+27 13 741 6100",
+        "web": "http://doctors-hospitals-medical-cape-town-south-africa.blaauwberg.net/hospitals_clinics_state_hospitals/state_public_hospitals_clinics_mpumalanga_south_africa/rob_ferreira_hospital_nelspruit_mbombela_mpumalanga_south_africa",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          30.97111,
+          -25.4755
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pelonomi Hospital",
+        "address": " 121 Dr Belcher Rd, Heidedal, Bloemfontein, 9301, South Africa",
+        "contact": "+27 51 405 1911" ,
+        "web": "https://www.ufs.ac.za/health/departments-and-divisions/obstetrics-and-gynaecology-home/units/pelonomi-tertiary-hospital",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.24572,
+          -29.1413
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Klerksdorp Hospital",
+        "address": "Neserhof, Klerksdorp, 2571, South Africa",
+        "contact": "+27 18 406 4600",
+        "web": "http://doctors-hospitals-medical-cape-town-south-africa.blaauwberg.net/hospitals_clinics_state_hospitals/state_public_hospitals_clinics_north_west_province_south_africa/klerksdorp_hospital_klerksdorp_north_west_south_africa",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          26.663295,
+          -26.878627
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kimberly Hospital",
+        "address": "Du Toitspan Road, Belgravia, Kimberley, Northern Cape, South Africa",
+        "contact": " 	+27 53 802 2124",
+        "web": "http://doctors-hospitals-medical-cape-town-south-africa.blaauwberg.net/hospitals_clinics_state_hospitals/state_public_hospitals_clinics_northern_cape_south_africa/kimberley_hospital_complex_kimberley_northern_cape_south_africa",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.773,
+          -28.7462
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tygerberg Hospital",
+        "address": " Francie van Zijl Avenue, Tygerberg, 7505, South Africa",
+        "contact": "+27 21 938 5570",
+        "web": "https://www.westerncape.gov.za/your_gov/153/contact-us",
+        "comment":"No appointment needed prior. Doctors Covid-19 test referral note is a requirement",
+        "source": "https://sacoronavirus.co.za/contact/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          18.611569,
+          -33.9147
+        ]
+      }
+    },
+
+
+    /*gift of givers testing centers
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "https://www.ticketprodome.co.za/",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": "https://www.sabcnews.com/sabcnews/gift-of-the-givers-set-up-coronavirus-testing-facilities-in-johannesburg/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "",
+        "address": "",
+        "contact": "",
+        "web": "",
+        "Appointment necessary?": "",
+        "Requirements":" ",
+        "source": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          ,
+          
+        ]
+      }
+    },  
+      */
+
   ]
 }
